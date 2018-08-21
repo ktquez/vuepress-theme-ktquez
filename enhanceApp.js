aa56a70fd2f745bef3bbeda0f916a0dd797f84ad
Alan Albuquerque (ktquez) <ktquez@gmail.com>
2018-08-21 17:54:16 -0300

    Add enhanceApp

---
+++ enhanceApp.js
@@ -0,0 +1,25 @@
+import { filterPages, sortArrayByProp } from '@theme/services/utils'
+import { post as PostTransformer } from '@theme/transformers/post'
+import Translation from '@theme/plugins/Translation'
+
+export default ({ Vue, siteData }) => {
+  Vue.prototype.$themeConfig = siteData.themeConfig
+  Vue.prototype.$posts = getPosts(siteData.pages)
+  Vue.prototype.$categories = getCategories(siteData.pages)
+  Vue.prototype.$authors = getAuthors(siteData.pages)
+
+  Vue.use(Translation)
+}
+
+function getPosts (pages) {
+  const posts = filterPages(pages, 'post', true)
+  return sortArrayByProp(PostTransformer(posts), 'created_time', 'desc')
+}
+
+function getAuthors (pages) {
+  return filterPages(pages, 'author')
+}
+
+function getCategories (pages) {
+  return filterPages(pages, 'category')
+}
