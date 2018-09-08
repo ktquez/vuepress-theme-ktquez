ae62a0c8885f90791ce5fafff6019d472510d06b
Alan Albuquerque (ktquez) <ktquez@gmail.com>
2018-09-07 23:06:29 -0300

    FIx: get themeConfig in service utils

--- services/utils.js
+++ services/utils.js
@@ -3,7 +3,7 @@ import config from '@source/.vuepress/config'
 export function getImagePost (path, source) {
   const slug = getSlug(path)
   const date = new Date(source.frontmatter.created_at)
-  return themeConfig.cdn ? `${themeConfig.cdn}/${date.getFullYear()}/${date.getMonth() + 1}/${slug}` : `/images/posts/${date.getFullYear()}/${date.getMonth() + 1}/${slug}`
+  return config.themeConfig.cdn ? `${config.themeConfig.cdn}/${date.getFullYear()}/${date.getMonth() + 1}/${slug}` : `/images/posts/${date.getFullYear()}/${date.getMonth() + 1}/${slug}`
 }
 
 export function getSlug (path) {
